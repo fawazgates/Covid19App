ef0dd0428e71eddf5f08007a86f1271527a2aad1
fawaz <fawaz.ajja@gmail.com>
2024-01-24 13:12:17 +0700

    Initial Commit

---
+++ Covid-19App/Covid_19AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Covid_19AppApp.swift
+//  Covid-19App
+//
+//  Created by User on 24/01/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Covid_19AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
